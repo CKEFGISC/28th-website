d2a8ee7243fe9f90eca3c98f9112e6dc708ba05b
Lily Kensa <berhew.cheng@gmail.com>
2023-11-22 23:17:45 +0800

    Adjust the Size of the Slides Section

--- src/pages/courses/components/Slides.js
+++ src/pages/courses/components/Slides.js
@@ -5,8 +5,8 @@ export default function Slides(props) {
       className="slides"
       title={props.title}
       src={`${props.url}/embed`} 
-      width={props.width || 400}
-      height={props.height || 340} 
+      width={props.width || 560}
+      height={props.height || 410} 
       style={{ borderRadius: 6, maxWidth: "98%", maxHeight: "50dvw" }}
       allowFullScreen
     ></iframe>
